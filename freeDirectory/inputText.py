c2fccc4820e0d4c15e9fbd251fccba1ddcb080e5
SerhiiStakhorsky <svhorsky@gmail.com>
2024-04-17 22:55:12 +0300

    change register

--- freeDirectory/inputText.py
+++ freeDirectory/inputText.py
@@ -2,6 +2,6 @@ txt = "The best things in life are free!"
 print(txt)
 inputtext = input("Search text:")
 if inputtext in txt:
-  print("Yes, '" + inputtext + "' is present.")
+  print("Yes, '" + inputtext.capitalize() + "' is present.")
 else:
-    print("No, '" + inputtext + "' is not present.")
+    print("No, '" + inputtext.capitalize() + "' is not present.")
