ea9946d8ede6861c50c9c293942eb26c92451324
SerhiiStakhorsky <svhorsky@gmail.com>
2024-04-20 18:02:23 +0300

    2.1homework - check input

--- 2_homework/2.1_homework.py
+++ 2_homework/2.1_homework.py
@@ -1,7 +1,13 @@
 inputNumber = input("Print the number:")
 print("Your number is", inputNumber)
-inputNumber = int(inputNumber)
-print (inputNumber // 1000)
-print(inputNumber % 1000 // 100)
-print (inputNumber % 100 // 10 )
-print(inputNumber % 10 // 1)
+try:
+    inputNumber = int(inputNumber)
+    if  999 < inputNumber < 10000:
+        print (inputNumber // 1000)
+        print (inputNumber % 1000 // 100)
+        print (inputNumber % 100 // 10 )
+        print (inputNumber % 10 // 1)
+    else:
+        print ("It`s not four-digit number");
+except  ValueError:
+    print("It`s not four-digit number");
