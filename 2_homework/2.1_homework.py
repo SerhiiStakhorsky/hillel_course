626a494d1788c9bac0c5b5a098e9d40af599495f
SerhiiStakhorsky <svhorsky@gmail.com>
2024-04-21 18:08:43 +0300

    2.1homework - change register and symbols

--- 2_homework/2.1_homework.py
+++ 2_homework/2.1_homework.py
@@ -1,13 +1,6 @@
-inputNumber = input("Print the number:")
-print("Your number is", inputNumber)
-try:
-    inputNumber = int(inputNumber)
-    if  999 < inputNumber < 10000:
-        print (inputNumber // 1000)
-        print (inputNumber % 1000 // 100)
-        print (inputNumber % 100 // 10 )
-        print (inputNumber % 10 // 1)
-    else:
-        print ("It`s not four-digit number.");
-except  ValueError:
-    print("It`s not four-digit number.");
+input_number = int(input("Input four-digit number:"))
+print("Your number is", input_number)
+print (input_number // 1000)
+print (input_number % 1000 // 100)
+print (input_number % 100 // 10)
+print (input_number % 10 // 1)
