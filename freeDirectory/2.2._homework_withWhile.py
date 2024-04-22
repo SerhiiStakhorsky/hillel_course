985bd6125d3c5d299ba2773a12c85c299c782362
SerhiiStakhorsky <svhorsky@gmail.com>
2024-04-22 21:21:42 +0300

    2.2homework - punctuation corrected

--- freeDirectory/2.2._homework_withWhile.py
+++ freeDirectory/2.2._homework_withWhile.py
@@ -10,7 +10,7 @@ while True:
                  ((input_number % 100) // 10) * 1000 +
                  ((input_number % 1000) // 100) * 100 +
                  ((input_number % 10000) // 1000) * 10 +
-                 ((input_number // 10000)))
+                 (input_number // 10000))
             print("Reverse number = ", x)
             break
     except ValueError:
