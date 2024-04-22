8a3d182e12ef4463051a521d62cd11d5993264bf
SerhiiStakhorsky <svhorsky@gmail.com>
2024-04-22 20:04:00 +0300

    2.2homework - check it's 5'digit number

--- freeDirectory/2.2._homework_withWhile.py
+++ freeDirectory/2.2._homework_withWhile.py
@@ -1,17 +1,17 @@
 while True:
     try:
         input_number = int(input("Input five-digit number:"))
-        if 9999 < input_number < 100000:
-            print ("Your number is", input_number)
+        if len(str(input_number)) != 5:
+            print("It's not a five digit number")
+            continue
+        else:
+            print("Your number is", input_number)
             x = (((input_number % 10) // 1) * 10000 +
                  ((input_number % 100) // 10) * 1000 +
                  ((input_number % 1000) // 100) * 100 +
                  ((input_number % 10000) // 1000) * 10 +
                  ((input_number // 10000)))
-            print ("Reverse number = ",x)
+            print("Reverse number = ", x)
             break
-        else:
-            print ("It's not a five digit number")
-            continue
     except ValueError:
         print("It's not a number")
