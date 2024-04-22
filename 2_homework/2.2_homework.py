64d74f89ff2055c5f7e32fa0f11eceed5cf9bb5c
SerhiiStakhorsky <svhorsky@gmail.com>
2024-04-22 19:34:14 +0300

    2.2homework - add variable and print reverse number

--- 2_homework/2.2_homework.py
+++ 2_homework/2.2_homework.py
@@ -1,7 +1,8 @@
 input_number = int(input("Input five-digit number:"))
-print("Your number is", input_number)
-print (input_number % 10 // 1,
-       input_number % 100 // 10,
-       input_number % 1000 // 100,
-       input_number % 10000 // 1000,
-       input_number // 10000)
+print ("Your number is", input_number)
+x = (((input_number % 10) // 1) * 10000 +
+     ((input_number % 100) // 10) * 1000 +
+     ((input_number % 1000) // 100) * 100 +
+     ((input_number % 10000) // 1000) * 10 +
+     ((input_number // 10000)))
+print (x)
