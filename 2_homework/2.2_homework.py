97f2b9e2755f2dfbc67444b0553eeceb366b53d7
SerhiiStakhorsky <svhorsky@gmail.com>
2024-04-22 21:16:57 +0300

    2.2homework - spaces removed

--- 2_homework/2.2_homework.py
+++ 2_homework/2.2_homework.py
@@ -1,8 +1,8 @@
 input_number = int(input("Input five-digit number:"))
-print ("Your number is", input_number)
+print("Your number is", input_number)
 x = (((input_number % 10) // 1) * 10000 +
      ((input_number % 100) // 10) * 1000 +
      ((input_number % 1000) // 100) * 100 +
      ((input_number % 10000) // 1000) * 10 +
      ((input_number // 10000)))
-print (x)
+print(x)
