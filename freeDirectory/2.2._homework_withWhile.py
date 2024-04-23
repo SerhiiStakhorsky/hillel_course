d86928762a73c6ac7e1a0f52dda8e67d768d19b0
SerhiiStakhorsky <svhorsky@gmail.com>
2024-04-23 23:00:55 +0300

    2.2_homework - deleted break, continue

--- freeDirectory/2.2._homework_withWhile.py
+++ freeDirectory/2.2._homework_withWhile.py
@@ -3,7 +3,6 @@ while True:
         input_number = int(input("Input five-digit number:"))
         if len(str(input_number)) != 5:
             print("It's not a five digit number")
-            continue
         else:
             print("Your number is", input_number)
             x = (((input_number % 10) // 1) * 10000 +
@@ -12,6 +11,5 @@ while True:
                  ((input_number % 10000) // 1000) * 10 +
                  (input_number // 10000))
             print("Reverse number = ", x)
-            break
     except ValueError:
         print("It's not a number")
