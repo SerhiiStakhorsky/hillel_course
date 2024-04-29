f519f3996bcb12f7f963812ef84526d4b51c3997
SerhiiStakhorsky <svhorsky@gmail.com>
2024-04-29 23:28:23 +0300

    22_homework - len(lst2) if ==0

--- 04_homework/22_homework.py
+++ 04_homework/22_homework.py
@@ -1,7 +1,10 @@
 import random
 lst1 = ()
-add_element = random.randint(2, 9)
+add_element = random.randint(0, 9)
 lst2 = [random.randint(1, 10) for i in range(add_element)]
-summ_even_element = sum(lst2[i] for i in range(0, len(lst2), 2))
-final_result = summ_even_element * lst2[-1]
-print(lst2, "=", final_result)
+if len(lst2) ==0:
+    print(lst2, "=", 0)
+else:
+    summ_even_element = sum(lst2[i] for i in range(0, len(lst2), 2))
+    final_result = summ_even_element * lst2[-1]
+    print(lst2, "=", final_result)
