1715b2f4002c48260f753bfcca5a88e37166624c
SerhiiStakhorsky <svhorsky@gmail.com>
2024-05-04 16:52:39 +0300

    5.1_homework - has_double_underscore

--- 05_homework/5.1_homework.py
+++ 05_homework/5.1_homework.py
@@ -4,6 +4,7 @@ import keyword
 user_variable_name = input("Print text: ")
 punctuation = string.punctuation.replace("_", "")
 banned_keywords = keyword.kwlist
+has_double_underscore = user_variable_name.count("__") >= 1 and user_variable_name.strip("_") == ""
 
 negative_contractions = [
     user_variable_name[0].isdigit(),
@@ -11,10 +12,9 @@ negative_contractions = [
     any(char in punctuation for char in user_variable_name),
     user_variable_name in banned_keywords,
     " " in user_variable_name,
-    user_variable_name.count("_") >1
 ]
 
-if any(negative_contractions):
+if has_double_underscore or any(negative_contractions):
     print(False)
 else:
     print(True)
