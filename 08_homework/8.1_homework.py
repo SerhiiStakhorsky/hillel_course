59e2f619af71ba3302c57a42aacaef3f97dcf541
SerhiiStakhorsky <svhorsky@gmail.com>
2024-05-20 23:22:51 +0300

    8.1_homework - add

---
+++ 08_homework/8.1_homework.py
@@ -0,0 +1,14 @@
+def add_one(digits):
+    number = int(''.join(map(str, digits)))
+    number += 1
+    result = [int(digit) for digit in str(number)]
+    return result
+
+
+if __name__ == "__main__":
+    assert add_one([1, 2, 3, 4]) == [1, 2, 3, 5], "Test 1 failed"
+    assert add_one([9, 9, 9]) == [1, 0, 0, 0], "Test 2 failed"
+    assert add_one([0]) == [1], "Test 3 failed"
+    assert add_one([9]) == [1, 0], "Test 4 failed"
+
+    print("Ok")
