171188a266d312ec51c54b5edefda74f6b5d347b
SerhiiStakhorsky <svhorsky@gmail.com>
2024-05-29 23:57:50 +0300

    12.1_homework - add

---
+++ 12_homework/12_1_homework/12.1_homework.py
@@ -0,0 +1,13 @@
+import codecs
+import re
+
+
+def delete_html_tags(html_file, result_file='cleaned.txt'):
+    with codecs.open(html_file, 'r', 'utf-8') as file:
+        html_content = file.read()
+    cleaned_text = re.sub(r'<[^>]*>', '', html_content)
+    with codecs.open(result_file, 'w', 'utf-8') as file:
+        file.write(cleaned_text)
+
+
+delete_html_tags('draft.html', 'cleaned.txt')
